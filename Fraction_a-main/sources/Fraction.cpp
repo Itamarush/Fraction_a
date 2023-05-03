b65bdbd3d2797e7686aa68ee356401c45c32b73a
Itamarush <itamar.guetta@gmail.com>
2023-05-03 22:12:54 +0300

    a

---
+++ Fraction_a-main/sources/Fraction.cpp
@@ -0,0 +1,94 @@
+#include "Fraction.hpp"
+
+namespace ariel{
+
+    Fraction::Fraction(int numerator , int denominator)
+    {
+        this-> numerator = numerator;
+        this-> numerator = denominator;
+
+    }
+
+    Fraction::Fraction()
+    {
+        this-> numerator = 0;
+        this-> numerator = 1;
+    }
+     
+    bool operator>(const Fraction& frac1, float num)
+    {
+        return false;
+    };
+
+    bool operator>=(const Fraction& frac1,const Fraction& frac2)
+    {
+        return false;
+    };
+    ostream& operator<<(ostream& ostr, const Fraction& frac);
+
+    Fraction operator+(const Fraction& frac1, const Fraction& frac2)
+    {
+        return Fraction();
+    };
+
+    Fraction operator+(const Fraction &frac1, float num)
+    {
+        return Fraction();
+    };
+
+    Fraction operator-(const Fraction& frac1 , const Fraction& frac2)
+    {
+        return Fraction();
+    };
+
+    Fraction operator-(const Fraction& frac1 , int num)
+    {
+        return Fraction();
+    };
+
+    Fraction operator*(const Fraction& fraction, float value)
+    {
+        return Fraction();
+    };
+
+    Fraction operator*(float value, const Fraction& fraction)
+    {
+        return Fraction();
+    };
+
+    Fraction operator/(const Fraction& fraction, float value)
+    {
+        return Fraction();
+    };
+
+    Fraction operator/(float value, const Fraction& fraction)
+    {
+        return Fraction();
+    };
+
+    // Fraction operator/(const Fraction& other) const
+    // { 
+    // return Fraction();
+    // };
+
+    Fraction Fraction::operator++()
+    {
+        return Fraction();
+    };
+
+    Fraction Fraction::operator++(int num)
+    {
+        return Fraction();
+    };
+
+    Fraction Fraction::operator--()
+    {
+        return Fraction();
+    };
+
+    Fraction Fraction::operator--(int num)
+    {
+        return Fraction();
+    };
+
+}
---
+++ Fraction_a-main/sources/Fraction.hpp
@@ -0,0 +1,41 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+namespace ariel
+{
+    class Fraction{
+
+    public:
+    int numerator;
+    int denominator;
+    
+    Fraction();
+    Fraction(int numerator, int denominator);
+    Fraction(float num);
+    
+    int getNum() const;
+    int getDen() const;
+
+    friend bool operator>(const Fraction& frac1, float num);    
+    friend bool operator>=(const Fraction& frac1,const Fraction& frac2);
+    friend ostream& operator<<(ostream& ostr, const Fraction& frac);
+    friend Fraction operator+(const Fraction& frac1, const Fraction& frac2);
+    friend Fraction operator+(const Fraction &frac1, float num);
+    friend Fraction operator-(const Fraction& frac1 , const Fraction& frac2);
+    friend Fraction operator-(const Fraction& frac1 , int num);
+    friend Fraction operator*(const Fraction& fraction, float value);
+    friend Fraction operator*(float value, const Fraction& fraction);
+    friend Fraction operator/(const Fraction& fraction, float value);
+    friend Fraction operator/(float value, const Fraction& fraction);
+    Fraction operator/(const Fraction& other) const;
+    Fraction operator++();
+    Fraction operator++(int num);
+    Fraction operator--();
+    Fraction operator--(int num);
+
+    };
+
+}
+   
+
